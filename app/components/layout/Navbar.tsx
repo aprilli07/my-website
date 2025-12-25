6d2a281f5345d8593431c27b8880aed2bf29571c
April <april.taotao.li07@gmail.com>
2025-12-25 11:55:19 -0500

    navbar fix

--- app/components/layout/Navbar.tsx
+++ app/components/layout/Navbar.tsx
@@ -6,12 +6,10 @@ export default function NavBar() {
     const [isOpen, setIsOpen] = useState(false);
 
     const navItems = [
-        { name: "Hero", href: "#hero" },
+        { name: "Experience", href: "#experience" },
         { name: "Projects", href: "#projects" },
         { name: "Contact", href: "#contact" },
     ];
-
-    {/* { name: "Experience", href: "#experience" }, */}
 
     return (
         <header className="sticky top-0 z-50 border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
